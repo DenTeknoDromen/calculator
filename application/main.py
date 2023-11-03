25a25e7441483a1bec8fc1e0987c3b79a39e022b
Tobias Jansson <tobias.janssn@gmail.com>
2023-11-03 23:51:18 +0100

    pre gui

--- application/main.py
+++ application/main.py
@@ -1,33 +1,37 @@
-# Siffer input
-# Enkla beräkningar
 # GUI med input
 
-operators = ["+", "-", "/", "*", 
+# Negativa tal och exponenter funkar inte, lös med gui
+
+operators = ["+", "-", "/", "*",
              "**", "%", "(", ")"]
 
+
 def make_float(input):
-    try:
-        input = float(input)
-    except Exception as e:
-        input = None
-        print("Wrong input")
+    for x in range(len(input)):
+        try:
+            input[x] = float(input[x])
+        except Exception as e:
+            if input[x] in operators:
+                input[x] = str(input[x])
     return input
 
-def get_operand(inputs, operand):
-    if operand == "+":
-        inputs[0] += inputs[1]
-    elif operand == "-":
-        inputs[0] -= inputs[1]
-    elif operand == "*":
-        inputs[0] *= inputs[1]
-    elif operand == "/":
-        inputs[0] /= inputs[1]
-    elif operand == "**":
-        inputs[0] **= inputs[1]        
-    elif operand == "%":
-        inputs[0] %= inputs[1]        
 
-    return inputs[0]
+def get_operand(a, b, operand):
+    if operand == "+":
+        a += b
+    elif operand == "-":
+        a -= b
+    elif operand == "*":
+        a *= b
+    elif operand == "/":
+        a /= b
+    elif operand == "**":
+        a **= b
+    elif operand == "%":
+        a %= b
+
+    return a
+
 
 def parse_input(input):
     index = 0
@@ -43,4 +47,25 @@ def parse_input(input):
     return output
 
 
-print(parse_input("55+55-5.5"))
+def parse_output(indata):
+    try:
+        if int(indata) == float(indata):
+            output = int(indata)
+        else:
+            output = float(indata)
+    except Exception as e:
+        print("Det här är fel")
+        output = str(indata)
+    return output
+
+
+def main(input):
+    lst_input = parse_input(input)  # placeholder
+    lst_input = make_float(lst_input)
+    output = get_operand(lst_input[0], lst_input[2], lst_input[1])
+    output = parse_output(output)
+    return output
+
+
+while True:
+    print(main(input("> ")))
