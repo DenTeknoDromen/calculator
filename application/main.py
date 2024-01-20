671c7d76e480aec182cc9902007380565bff58ba
Tobias Jansson <tobias.janssn@gmail.com>
2024-01-20 20:26:32 +0100

    updated main class

--- application/main.py
+++ application/main.py
@@ -1,5 +1,5 @@
 import math
-# GUI med input
+import re
 
 # Negativa tal och exponenter funkar inte, lös med gui
 # Math decimal/length check
@@ -9,7 +9,26 @@ import math
 operators = ["+", "-", "/", "÷", "×", "^", 
              "**", "%", "(", ")"]
 
+# Converts string input to list with numbers and operators as string
+def parse_input(str_input):
+    lst_input = re.split("([^0-9π.])", str_input)
+    if lst_input[0] == "":
+        lst_input[0] = "0"
 
+    if lst_input[-1] == "":
+         lst_input[-1] = "0"
+
+    return lst_input
+
+# Checks for edgecases that needs to be handled before making calculations
+def check_edgecases(indata):
+    if "π" in indata:
+        i = indata.index("π")
+        indata[i] = math.pi  
+    return indata
+
+# Converts string integers to float numbers
+# Returns list with operators as string and numbers as float
 def make_float(input):
     for x in range(len(input)):
         try:
@@ -19,7 +38,7 @@ def make_float(input):
                 input[x] = str(input[x])
     return input
 
-
+# Makes calulations according to operators
 def get_operand(a, b, operand):
     if operand == "+":
         a += b
@@ -37,20 +56,14 @@ def get_operand(a, b, operand):
     # Math decimalcheck här 
     return a
 
-def parse_input(input):
-    index = 0
-    output = []
-    max = len(input)
-    for x in range(max):
-        if input[x] in operators:
-            output.append(input[index:x])
-            output.append(input[x])
-            index = x + 1
-        if x == max - 1:
-            output.append(input[index:max])
+
+def make_calc(lst_input):
+    output = lst_input[0]
+    for i in range(2, len(lst_input), 2):
+        output = get_operand(output, lst_input[i], lst_input[i-1])
     return output
 
-
+# Converts list to string for output
 def parse_output(indata):
     try:
         if int(indata) == float(indata):
@@ -62,20 +75,14 @@ def parse_output(indata):
         output = str(indata)
     return output
 
-def edgecase(indata):
-    if "π" in indata:
-        i = indata.index("π")
-        indata[i] = math.pi  
-    return indata
-
 def main(input):
-    lst_input = parse_input(input)  # placeholder
-    lst_input = edgecase(lst_input)
+    lst_input = parse_input(input)
+    print(lst_input)
+    lst_input = check_edgecases(lst_input)
     lst_input = make_float(lst_input)
-    output = get_operand(lst_input[0], lst_input[2], lst_input[1])
+    output = make_calc(lst_input)
     output = parse_output(output)
     return output
 
-
-# while True:
-#     print(main(input("> ")))
+if __name__ == "__main__":
+    print(main("5÷5+5"))
